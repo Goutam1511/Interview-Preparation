b96406e767cde38f7546a3dcaceced59ae9dd7bd
Goutam Bose <gotubose@gmail.com>
2020-06-01 04:08:56 +0530

    Update Single Element in a Sorted Array.cpp

--- Algorithms/Geeks for Geeks Practice Questions/Single Element in a Sorted Array.cpp
+++ Algorithms/Geeks for Geeks Practice Questions/Single Element in a Sorted Array.cpp
@@ -5,6 +5,17 @@ using namespace std;
 /* Given a sorted array consisting of only integers where every element appears exactly twice,
  * except for one element which appears exactly once.
  * Find this single element that appears only once.
+ * Solution: Since array is sorted, we can do binary search. The approach is that, while searching
+ * if nums[mid - 1] == nums[mid] and mid - 1 is even, then the single element is on right side & we
+ * can ignore searching left side. This is because, uptil mid every element has occurred twice and 
+ * hence mid - 1 is bound to be an even index. Same logic if nums[mid] == nums[mid + 1]. But if that
+ * doesn't happen, then a single element must be in the left side which has disrupted the indexing.
+ * So we should search only in left side.
+ * VISUAL DRY RUN : Input : [1, 1, 2, 2, 3, 4, 4]
+ * Note the indexes :        0  1  2  3  4  5  6
+ * Uptil the occurance of single element, in case of all the coupled elements, first of the pair was
+ * in even index and second in odd index. But after the occurance of single element, this reverses. 
+ * This fact is used as the logic to determine the bounds in binary search. 
  */
  
  class Solution {
