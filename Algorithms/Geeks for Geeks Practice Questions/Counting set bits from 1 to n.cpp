ad084837897f37d981ef88c978e896d906532675
Goutam Bose <gotubose@gmail.com>
2020-05-31 12:42:34 +0530

    Update Counting set bits from 1 to n.cpp

--- Algorithms/Geeks for Geeks Practice Questions/Counting set bits from 1 to n.cpp
+++ Algorithms/Geeks for Geeks Practice Questions/Counting set bits from 1 to n.cpp
@@ -1,5 +1,6 @@
 /* 
-Counting Bits
+Counting Set Bits from 1 to N
+
 Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their
 binary representation and return them as an array.
 
@@ -9,7 +10,6 @@ Output: [0,1,1]
 Example 2:
 Input: 5
 Output: [0,1,1,2,1,2]
-
 
 SOLUTION APPROACH : The number of set bit in a mumber is = (the set bits in number /2) + 1 if the number is odd.
 
