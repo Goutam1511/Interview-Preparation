f1ff0f61875be47f0972d4513010966c7d249357
Goutam Bose <gotubose@gmail.com>
2020-06-18 00:28:03 +0530

    Updating consistency of documentation

--- Algorithms/Geeks for Geeks Practice Questions/Partition Equal Subset Sum.cpp
+++ Algorithms/Geeks for Geeks Practice Questions/Partition Equal Subset Sum.cpp
@@ -1,29 +1,21 @@
 /*
-Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of 
+elements in both subsets is equal.
 
 Note:
-
     Each of the array element will not exceed 100.
     The array size will not exceed 200.
-
- 
 
 Example 1:
 
 Input: [1, 5, 11, 5]
-
 Output: true
-
 Explanation: The array can be partitioned as [1, 5, 5] and [11].
-
- 
 
 Example 2:
 
 Input: [1, 2, 3, 5]
-
 Output: false
-
 Explanation: The array cannot be partitioned into equal sum subsets.
 
 Solution : The problem is similar to Subset Sum : (https://www.geeksforgeeks.org/subset-sum-problem-dp-25/). The only exception is
@@ -61,7 +53,6 @@ target=6
 2   T    F    T    T    T    T    T
 
 End output : table[n][sum]
-
 */
 
 class Solution {
