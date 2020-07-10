c686502c19c3184f8520c8875d0b89ccc34ca3e7
Goutam Bose <gotubose@gmail.com>
2020-07-10 20:15:04 +0530

    Updating Documentation to make it look consistent

--- Data Structures/Linked List/Flatten a Multi-Level Linked List.cpp
+++ Data Structures/Linked List/Flatten a Multi-Level Linked List.cpp
@@ -10,11 +10,11 @@ public:
 /*
 Problem Link : https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
 
-You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
+You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, 
+which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, 
+and so on, to produce a multilevel data structure, as shown in the example below.
 
 Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the first level of the list.
-
- 
 
 Example 1:
 
