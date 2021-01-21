cd7adb418ff82486f201a3f614705b21f79c4320
Goutam Bose <gotubose@gmail.com>
2021-01-21 21:35:17 +0530

    Create Find the Most Competitive Subsequence.cpp

---
+++ Data Structures/Stack/Find the Most Competitive Subsequence.cpp
@@ -0,0 +1,44 @@
+/*
+Given an integer array nums and a positive integer k, return the most competitive subsequence of nums of size k.
+An array's subsequence is a resulting sequence obtained by erasing some (possibly zero) elements from the array.
+We define that a subsequence a is more competitive than a subsequence b (of the same length) if in the first position where a and b differ,
+subsequence a has a number less than the corresponding number in b. For example, [1,3,4] is more competitive than [1,3,5] because the first 
+position they differ is at the final number, and 4 is less than 5.
+
+Example 1:
+
+Input: nums = [3,5,2,6], k = 2
+Output: [2,6]
+Explanation: Among the set of every possible subsequence: {[3,5], [3,2], [3,6], [5,2], [5,6], [2,6]}, [2,6] is the most competitive.
+Example 2:
+
+Input: nums = [2,4,3,3,5,4,9,6], k = 4
+Output: [2,3,3,4]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+1 <= k <= nums.length
+
+Solution : Use stack to build a monotonic increasing subsequence while iterating from left to right. For doing that while iterating, in case we hit a number
+that is lesser than the numbers already on stack, we start popping the greater numbers from stack. However, since we are building a subsequence of size k, we
+need to always have at least k numbers in stack and hence we can pop only (nums.size() - k) numbers atmost.
+*/
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        vector<int> stack;
+        int nums_to_delete = nums.size()-k;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!stack.empty() && nums[i] < stack.back() && nums_to_delete) {
+                stack.pop_back();
+                nums_to_delete--;
+            }
+            stack.push_back(nums[i]);
+        }
+        return vector<int>(stack.begin(), stack.begin()+k);
+    }
+};
