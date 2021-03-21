a4dc6010d8ea7bdfd583a6fd49abe7f2af28782e
Goutam Bose <gotubose@gmail.com>
2021-03-21 20:25:30 +0530

    Reordered power of 2 | LeetCode

---
+++ Algorithms/Geeks for Geeks Practice Questions/Reordered power of 2.cpp
@@ -0,0 +1,46 @@
+/*
+Starting with a positive integer N, we reorder the digits in any order (including the original order) such that the leading digit is not zero.
+Return true if and only if we can do this in a way such that the resulting number is a power of 2.
+
+Example 1:
+
+Input: 1
+Output: true
+Example 2:
+
+Input: 10
+Output: false
+Example 3:
+
+Input: 16
+Output: true
+Example 4:
+
+Input: 24
+Output: false
+Example 5:
+
+Input: 46
+Output: true
+
+Note:
+1 <= N <= 10^9
+
+Solution : Sort the number by digits. And for each power of 2 upto 2^32, sort the power of 2 by digits and check if it matches the number.
+*/
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int N) {
+        string N_str = sorted_num(N);
+        for (int i = 0; i < 32; i++)
+            if (N_str == sorted_num(1 << i)) return true;
+        return false;
+    }
+    
+    string sorted_num(int n) {
+        string res = to_string(n);
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
