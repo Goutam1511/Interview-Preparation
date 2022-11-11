672320abfb541f49e0b1227338916218f2cf3db4
Goutam Bose <gotubose@gmail.com>
2022-11-11 08:37:23 +0530

    26. Remove Duplicates from Sorted Array | 2 pointers | LC

---
+++ Algorithms/Geeks for Geeks Practice Questions/26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,47 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same.
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums.
+More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result.
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Constraints:
+
+1 <= nums.length <= 3 * 10^4
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+
+Solution : Two pointers, one pointing to the last element moved and helping to determine the next position of the next unique element & the other iterating the
+array. Same as https://leetcode.com/problems/remove-element/ 
+Time Complexity : O(n)
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[j] != nums[i]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
